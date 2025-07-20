67d44091db0c685ed3c5e3693626e221662bcc03
Anna Abdiyu <annaply156@gmail.com>
2025-07-20 12:09:00 +0300

    final

--- 0x03-Unittests_and_integration_tests/test_utils.py
+++ 0x03-Unittests_and_integration_tests/test_utils.py
@@ -5,6 +5,7 @@
 from unittest import TestCase
 from parameterized import parameterized
 from utils import access_nested_map
+from unittest.mock import patch, Mock
 
 
 class TestAccessNestedMap(TestCase):
@@ -27,3 +28,21 @@ class TestAccessNestedMap(TestCase):
         """Test access_nested_map raises KeyError for invalid paths."""
         with self.assertRaises(KeyError):
             access_nested_map(nested_map, path)
+            
+class TestGetJson(TestCase):
+    """Test class for get_json."""
+
+    @parameterized.expand([
+        ("http://example.com", {"payload": True}),
+        ("http://holberton.io", {"payload": False}),
+    ])
+    def test_get_json(self, test_url, test_payload):
+        """Test get_json returns expected payload from mocked requests.get."""
+        with patch('utils.requests.get') as mock_get:
+            mock_response = Mock()
+            mock_response.json.return_value = test_payload
+            mock_get.return_value = mock_response
+
+            result = access_nested_map.get_json(test_url)
+            mock_get.assert_called_once_with(test_url)
+            self.assertEqual(result, test_payload)
--- 0x03-Unittests_and_integration_tests/utils.py
+++ 0x03-Unittests_and_integration_tests/utils.py
@@ -1,3 +1,10 @@
+import requests
+
+def get_json(url):
+    """GET JSON from URL."""
+    response = requests.get(url)
+    return response.json()
+
 def access_nested_map(nested_map, path):
     """Access a nested object in `nested_map` with `path`."""
     for key in path:
