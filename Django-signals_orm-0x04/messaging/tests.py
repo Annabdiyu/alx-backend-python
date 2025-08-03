ef11aed096af58d489e6f304a17cfcb73c2c411c
Anna Abdiyu <annaply156@gmail.com>
2025-08-03 20:58:36 +0300

    final

--- Django-signals_orm-0x04/messaging/tests.py
+++
@@ -1,9 +0,0 @@
-def get_thread(root_message):
-    return Message.objects.filter(
-        pk=root_message.pk
-    ).prefetch_related(
-        models.Prefetch(
-            'replies',
-            queryset=Message.objects.all().prefetch_related('replies')
-        )
-    ).first()
